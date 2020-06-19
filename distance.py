acc82bf132e2cbcab885e0d77ebbb20cf7fa2296
unknown <junhosk12@gmail.com>
2020-06-19 23:53:35 +0900

    edit

--- distance.py
+++ distance.py
@@ -1,8 +1,14 @@
 import math
 
-p1 = [4, 0]
-p2 = [6, 8]
-
-distance = math.sqrt( ((p1[0]-p2[0])**) + ((p1[1])-p2[1])**2)
-
-print(distance)
+print('First Point Axis (x1, y1) ')
+print("="*25)
+x1=float(input('Input the value of x1 '))
+y1=float(input('Input the value of y1 '))
+print('Second Point Axis (x1,y1) ')
+print("="*25)
+x2=float(input('Input the value of x2 '))
+y2=float(input('Input the value of y2 '))
+p1 = [x1, y1]
+p2 = [x2, y2]
+dist=math.sqrt(((p1[0]-p2[0])**2)+((p1[1]-p2[1])**2))
+print('The distance between the points are: ', dist)
